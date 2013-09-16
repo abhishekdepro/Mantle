76120a0d091d81adb930a797c2bf71bd52296a13
Abhishek De <abhishekde@hotmail.com>
2013-09-16 19:54:08 +0530

    next day

---
+++ forecast_for_nextday.py
@@ -0,0 +1,47 @@
+import urllib2
+import re
+import datetime
+
+city=raw_input('enter city')
+
+str1="&type=accurate&mode=xml&units=metric&cnt=2"
+str2=city+str1
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s" % str2
+source=urllib2.urlopen(url)
+tomorrow=str(datetime.date.today()+datetime.timedelta(days=1))
+htmltext=source.read()
+print("<------------------------WEATHER REPORT: "+city.upper()+" for "+tomorrow+" ------------>")
+
+# search for pattern using regular expressions (.+?)
+#if(htmltext.find(adate)!=-1):
+wind='<windDirection deg="(.+?)" code="(.+?)" name="(.+?)"/>'
+temp='<temperature day="(.+?)" min="(.+?)" max="(.+?)" night="(.+?)" eve="(.+?)" morn="(.+?)"/>'
+condition='<symbol number="(.+?)" name="(.+?)" var="(.+?)"/>'
+pattern_wind=re.compile(wind)
+pattern_temp=re.compile(temp)
+pattern_cond=re.compile(condition)
+try:
+# match pattern with htmltext
+	weather_winddirection=re.findall(pattern_wind,htmltext)
+	weather_temp=re.findall(pattern_temp,htmltext)
+	weather_cond=re.findall(pattern_cond,htmltext)
+	print "Overall Weather status: ",weather_cond[1][1]	
+	print "Temperature @Morning: ",weather_temp[1][5]
+	print "Temperature @Day: ",weather_temp[1][0]
+	print "Temperature @Evening: ",weather_temp[1][4]
+	print "Temperature @Night: ",weather_temp[1][3]
+	print ""
+	print "Max Temperature: ",weather_temp[1][2]
+	print "Min Temperature: ",weather_temp[1][1]	
+	
+	print ""
+		
+	if(weather_winddirection[1][2]!=""):	
+		print "Wind Direction: ",weather_winddirection[1][2]
+	
+except Exception:
+	print "Data unavailable!"
+#print "Min. Temperature is: ",weather_temp[0][2]," degree celcius"
+#print "Wind: ",weather_wind[0][1]
+#print "Humidity: ",weather_humidity[0]," %"
+#print "Weather Condition is : ",weather_condition[0][1]
